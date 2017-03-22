6bc347b041a9787f3a2737e2e7d1e6e88cd79b04
shiying1 <shiying1@ZB-PC0DLKP0.360buyAD.local>
2017-03-22 12:48:48 +0800

    testing instantiate

--- testapp.js
+++ testapp.js
@@ -1,12 +1,32 @@
 //createClient = require('./app/client/create-channel.js');
 //joinClient = require('./app/client/join-channel.js');
-//installClient = require('./app/client/install-chaincode.js');
-//instantiateClient = require('./app/client/instantiate-chaincode.js');
+installClient = require('./app/client/install-chaincode.js');
+instantiateClient = require('./app/client/instantiate-chaincode.js');
 invokeClient = require('./app/client/invoke-transaction.js');
 queryClient = require('./app/client/query.js');
 //queryBlockClient = require('./app/client/query-block.js');
 
-queryClient.queryTransaction('')
+
+//installClient.installChaincode(sendResponse);
+
+function sendResponse(result){
+	console.log('API: query result %s', JSON.stringify(result));
+	var all_good = true;
+	for (let num in result) {
+		if (result[num] == 'FAILED') {
+			all_good = all_good && false;
+		}
+	}
+	if (all_good) {
+		console.log('all good');
+	
+	}
+	else {
+		console.log('not all good');
+	}
+}
+
+instantiateClient.instantiateChaincode('org1')
 .then((result) => {
 	console.log('API: query result %s', JSON.stringify(result));
 	res.json(result); // return all amounts in JSON format
@@ -18,6 +38,37 @@ res.json('failed');
 console.error('API: query result %s', result);
 return 'failed';
 });
+
+
+/*
+queryClient.queryTransaction('ca36554c8856b81e946ac40efdaa175b8a5f405d3c351d034eb2f6218ada6f7d')
+.then((result) => {
+	console.log('API: query result %s', JSON.stringify(result));
+	res.json(result); // return all amounts in JSON format
+},
+(err) => {
+console.error('API: query result %s', result);
+res.json('failed');
+}).catch((err) => {
+console.error('API: query result %s', result);
+return 'failed';
+});
+*/
+
+/*
+queryClient.queryPeers('mychannel')
+.then((result) => {
+	console.log('API: query result %s', JSON.stringify(result));
+	res.json(result); // return all amounts in JSON format
+},
+(err) => {
+console.error('API: query result %s', result);
+res.json('failed');
+}).catch((err) => {
+console.error('API: query result %s', result);
+return 'failed';
+});
+*/
 
 /*
 invokeClient.invokeChaincode('')
