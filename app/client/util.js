d791a57104a5460df143624671664dafbfc093fb
shiying1 <shiying1@ZB-PC0DLKP0.360buyAD.local>
2017-03-31 16:22:38 +0800

    refactored

--- app/client/util.js
+++ app/client/util.js
@@ -36,13 +36,22 @@ module.exports.chaincodeVersion = 'v0';
 hfc.addConfigFile('./app/config/config.json');
 module.exports.ORGS = hfc.getConfigSetting('test-network');
 
-// Directory for file based KeyValueStore
-module.exports.storePathForOrg = function(org) {
-	return '/tmp/hfc-test-kvs' + '_' + org;
-};
 
-// Clean up KeyValueStore before new operations
-module.exports.cleanupDir = function(keyValStorePath) {
+// Function exports:
+module.exports.cleanupDir = cleanupDir;
+module.exports.checkProposalResponses = checkProposalResponses;
+module.exports.getKeyOfJson = getKeyOfJson;
+module.exports.getMspid = getMspid;
+module.exports.getOrgs = getOrgs;
+module.exports.getOrgNameByOrg = getOrgNameByOrg;
+module.exports.getValueOfJson = getValueOfJson;
+module.exports.readFile = readFile;
+module.exports.storePathForOrg = storePathForOrg;
+module.exports.throwError = throwError;
+
+
+//Clean up KeyValueStore before new operations
+function cleanupDir(keyValStorePath) {
 	var absPath = keyValStorePath;
 	//var absPath = path.join(process.cwd(), keyValStorePath);
 	deleteFolderRecursive(absPath);
@@ -51,83 +60,9 @@ module.exports.cleanupDir = function(keyValStorePath) {
 	deleteFolderRecursive('~/.hfc-key-store');
 };
 
-//Read Org from config
-module.exports.getOrgs = function getOrgs(ORGS) {
-	var orgs = [];
-	for (let key in ORGS) {
-		if (ORGS.hasOwnProperty(key) && typeof ORGS[key].peer1 !== 'undefined') {
-			orgs.push(key);
-		}
-	}
-	return orgs;
-}
-
-// Read Org Name from config
-module.exports.getOrgNameByOrg = function getOrgNameByOrg(ORGS, org) {
-	return ORGS[org].name;
-}
-
-// Read MSP ID from config
-module.exports.getMspid = function getMspid(ORGS, org) {
-	return ORGS[org].mspid;
-}
-
-// Process error by logging and throwing new error
-module.exports.throwError = function throwError(logger, err, description){
-	logger.error(description + err);
-	throw new Error(description + err);
-}
-
-// Read file
-module.exports.readFile = function readFile(path) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(path, (err, data) => {
-			if (!!err)
-				reject(new Error('Failed to read file ' + path + ' due to error: ' + err));
-			else
-				resolve(data);
-		});
-	});
-}
-
-
-// Check if directory or file exists
-// uses entire / absolute path from root
-function existsSync(absolutePath /*string*/) {
-	try  {
-		var stat = fs.statSync(absolutePath);
-		if (stat.isDirectory() || stat.isFile()) {
-			return true;
-		} else
-			return false;
-	}
-	catch (e) {
-		return false;
-	}
-};
-
-
-// Remove directory recursively
-function deleteFolderRecursive(path) {
-    var files = [];
-    if( fs.existsSync(path) ) {
-        files = fs.readdirSync(path);
-        files.forEach(function(file,index){
-            var curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) { // recurse
-                deleteFolderRecursive(curPath);
-            } else { // delete file
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(path);
-    }
-}
-
 
 //Check status code of all responses to 
 //ensure get valid response from all peers
-module.exports.checkProposalResponses = 
 function checkProposalResponses(results, proposal_type, logger) {
 	var all_good = true;
 
@@ -154,4 +89,110 @@ function checkProposalResponses(results, proposal_type, logger) {
 	}
 	
 	return all_good;
+}
+
+
+//Remove directory recursively
+function deleteFolderRecursive(path) {
+ var files = [];
+ if( fs.existsSync(path) ) {
+     files = fs.readdirSync(path);
+     files.forEach(function(file,index){
+         var curPath = path + "/" + file;
+         if(fs.statSync(curPath).isDirectory()) { // recurse
+             deleteFolderRecursive(curPath);
+         } else { // delete file
+             fs.unlinkSync(curPath);
+         }
+     });
+     fs.rmdirSync(path);
+ }
+}
+
+
+//Check if directory or file exists
+//uses entire / absolute path from root
+function existsSync(absolutePath /*string*/) {
+	try  {
+		var stat = fs.statSync(absolutePath);
+		if (stat.isDirectory() || stat.isFile()) {
+			return true;
+		} else
+			return false;
+	}
+	catch (e) {
+		return false;
+	}
+};
+
+
+//Return all keys matches keyword, not strictly
+function getKeyOfJson(jsonObj, keyword) {
+	var list = [];
+	for (let key in jsonObj) {
+		if (jsonObj.hasOwnProperty(key)) {
+			if (key.indexOf(keyword) === 0) {
+				list.push(key);
+			}
+		}
+	}
+	return list;
+}
+
+
+//Read MSP ID from config
+function getMspid(ORGS, org) {
+	return ORGS[org].mspid;
+}
+
+
+//Read Org from config
+function getOrgs(ORGS) {
+	return module.exports.getKeyOfJson(ORGS, 'org');
+}
+
+	
+// Read Org Name from config
+function getOrgNameByOrg(ORGS, org) {
+	return ORGS[org].name;
+}
+
+
+//Return all values matches keyword, not strictly
+function getValueOfJson(jsonObj, keyword) {
+	var list = [];
+	for (let key in jsonObj) {
+		if (jsonObj.hasOwnProperty(key)) {
+			if (key.indexOf(keyword) === 0) {
+				list.push(jsonObj[key]);
+			}
+		}
+	}
+	return list;
+}
+
+
+//Read file
+function readFile(path) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(path, (err, data) => {
+			if (!!err)
+				reject(new Error('Failed to read file ' + path + ' due to error: ' + err));
+			else
+				resolve(data);
+		});
+	});
+}
+
+
+//Directory for file based KeyValueStore
+function storePathForOrg(org) {
+	return '/tmp/hfc-test-kvs' + '_' + org;
+};
+
+
+//Process error by logging and throwing new error
+function throwError(logger, err, description){
+	logger.error(description + err);
+	throw new Error(description + err);
 }
