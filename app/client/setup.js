fd29cfb163af997227a9f6d828ca4a1b38e941ad
shiying1 <shiying1@ZB-PC0DLKP0.360buyAD.local>
2017-03-31 16:50:48 +0800

    refactored exports

--- app/client/setup.js
+++ app/client/setup.js
@@ -22,19 +22,175 @@ var Peer = require('fabric-client/lib/Peer.js');
 var EventHub = require('fabric-client/lib/EventHub.js');
 var util = require('./util.js');
 
-var logger = ClientUtils.getLogger('invoke-chaincode');
+var logger = ClientUtils.getLogger('setup-chain');
 
 
-// Simply and quick check port connectivity, so that
-// instantiate and invoke will not connect DOWN peer
-module.exports.getAlivePeer = function(ORGS, org) {
+// Export functions
+module.exports.getAlivePeer = getAlivePeer;
+module.exports.setupChain = setupChain;
+module.exports.setupChainWithEventbus = setupChainWithEventbus;
+module.exports.setupChainWithOnlyPrimaryPeer = setupChainWithOnlyPrimaryPeer;
+module.exports.setupChainWithAllPeers = setupChainWithAllPeers;
+
+
+//Set up the chain with orderer
+function addOrderer(chain, ORGS) {
+	chain.addOrderer(new Orderer(ORGS.orderer));
+}
+
+
+//Set up the chain with peer
+function addPeer(chain, ORGS, peerInfo, asPrimary) {
+	var url = peerInfo['requests'];
+	let peer = new Peer(url);
+	chain.addPeer(peer);
+	if (asPrimary) {
+		chain.setPrimaryPeer(peer);
+	}
+}
+
+
+//Set up the chain with eventhub
+function connectEventHub(eventhubs, allEventhubs, peerInfo) {
+	var eventsUrl = peerInfo['events'];
+	let eh = new EventHub();
+
+	eh.setPeerAddr(eventsUrl);
+	eh.connect();
+	eventhubs.push(eh);
+	allEventhubs.push(eh);	
+}
+
+
+function checkTheNext(peerList) {
+	// Looking for alive peer recursively and returning its url
+	
+	// Take one from the list each time, also removing it
+	var peer = popRandom(peerList);
+	try {
+		var peerUrl = peer['requests'];
+	} catch(err) {
+		var peerUrl = '';
+	}
+	
+	return isPortAlive(getIpFromEndpoint(peerUrl), getPortFromEndpoint(peerUrl))
+	.then((res) => {
+		logger.debug('Find alive peer %s and returning it.', peerUrl);
+		return peer;
+
+	}).catch((err) => {
+		logger.debug('Peer %s is not connected, try next.', peerUrl);
+		if (0 < peerList.length) {
+			return checkTheNext(peerList);
+		} else {
+			util.throwError(logger, err, 'Failed in getting any alive peer, returning nothing.');
+			return null;
+		}
+	});
+}
+
+
+function cleanupKeyValueStore(ORGS) {
+	var orgList = getOrgAll(ORGS);
+	for (let i in orgList) {
+		var orgName = util.getOrgNameByOrg(ORGS, orgList[i]);
+		util.cleanupDir(util.storePathForOrg(orgName));
+	}
+}
+
+
+//Simply and quick check port connectivity, so that
+//instantiate and invoke will not connect DOWN peer
+function getAlivePeer(ORGS, org) {
 	var peerList = getPeerByOrg(ORGS, 'org2');
 	return checkTheNext(peerList);
 }
 
 
-// initial a new chain for specific org with all peers
-module.exports.setupChain = function(client, ORGS, orgName, peerOrg) {
+function getIpFromEndpoint(endpoint) {
+	if (endpoint) {
+		return endpoint.split('//')[1].split(':')[0];
+	} else {
+		return null;
+	}
+}
+
+
+function getOrgAll(ORGS) {
+	// There may be multiple org in ORGS
+	return util.getKeyOfJson(ORGS, 'org');
+}
+
+
+function getPeerAll(ORGS) {
+	var list = [];
+	var orgList = getOrgAll(ORGS);
+	for (let i in orgList) {
+		list = list.concat(getPeerByOrg(ORGS, orgList[i]));
+	}
+	return list;
+}
+
+
+function getPeerByOrg(ORGS, org) {
+	var list = [];
+	// There may be multiple peers in an org
+	var valueOfPeers = util.getValueOfJson(ORGS[org], 'peer');
+	for (let i in valueOfPeers) {
+		list.push(valueOfPeers[i]);
+	}
+	return list;
+}
+
+
+function getPortFromEndpoint(endpoint) {
+	if (endpoint) {
+		return endpoint.split('//')[1].split(':')[1];
+	} else {
+		return null;
+	}
+}
+
+
+function getRandom(n,m){
+	// Equal possibility of generating integer in [n,m)
+	// Notice m is open and not included in output
+	return Math.floor(Math.random()*(m-n)+n);
+}
+
+
+function isPortAlive(host, port) {
+	logger.debug('Checking connectivity of %s:%s', host, port);
+	return new Promise((resolve, reject) => {
+		net.createConnection(port, host)
+		.on("connect", function() {
+			resolve(); 
+		}).on("error", function() {
+			reject();
+		});
+	}).catch((err) => {
+		reject();
+	});	
+}
+
+
+function popRandom(list) {
+	logger.debug('Poping cell randomly from %s', JSON.stringify(list));
+	if (list) {
+		var i = getRandom(0, list.length);
+		var component = list[i];
+		list.splice(i, 1);
+		logger.debug('Successfully pop %s, now list becomes %s', JSON.stringify(component), JSON.stringify(list));
+		return component;
+	} else {
+		logger.error('Empty list, return nothing.');
+		return null;
+	}
+}
+
+
+//initial a new chain for specific org with all peers
+function setupChain(client, ORGS, orgName, peerOrg) {
 	var chain = client.newChain(util.channel);
 	chain.addOrderer(new Orderer(ORGS.orderer));
 
@@ -56,40 +212,29 @@ module.exports.setupChain = function(client, ORGS, orgName, peerOrg) {
 
 
 
-//initial a new chain for specific org with only peer1 with eventbus
-module.exports.setupChainWithEventbus = function(client, eventhubs, allEventhubs, ORGS, orgName, peerOrg) {
-	// set up the chain with orderer
-	var chain = client.newChain(util.channel);
-	chain.addOrderer(new Orderer(ORGS.orderer));
+// Initial a new chain for specific org with one peer and connect to its eventbus
+function setupChainWithEventbus(client, eventhubs, allEventhubs, ORGS, peerInfo, asPrimary) {
+	try{
+		var chain = client.newChain(util.channel);
 
-	// set up the chain to use each org's 'peer1' for
-	// both requests and events
-	for (let key in ORGS) {
-		if (ORGS.hasOwnProperty(key) && typeof ORGS[key].peer1 !== 'undefined') {
-			let peer = new Peer(ORGS[key].peer1.requests);
-			chain.addPeer(peer);
-			if (!chain.isValidPeer(peer)) {
-				chain.addPeer(peer);
-				//console.debug('喔～ key is %s, org is %s', key, peerOrg);
-				if (key == peerOrg) {
-					chain.setPrimaryPeer(peer);
-				}
-			}
+		addOrderer(chain, ORGS);
+		addPeer(chain, ORGS, peerInfo, asPrimary);
+		connectEventHub(eventhubs, allEventhubs, peerInfo);
 
-			connectEventHub(eventhubs, allEventhubs, ORGS[key].peer1.events);
-			connectEventHub(eventhubs, allEventhubs, ORGS[key].peer2.events);
-		}
+		// Remove expired keys before enroll user
+		cleanupKeyValueStore(ORGS);
+
+		return chain;
+		
+	} catch(err) {
+		util.throwError(logger, err, 'Failed in setting up chain, check config file.');
+		return null;
 	}
-
-	// remove expired keys before enroll admin
-	util.cleanupDir(util.storePathForOrg(orgName));
-	
-	return chain;
 }
 
 
 //initial a new chain for specific org with only peer1 and as primary peer
-module.exports.setupChainWithOnlyPrimaryPeer = function(client, ORGS, orgName, peerOrg) {
+function setupChainWithOnlyPrimaryPeer(client, ORGS, orgName, peerOrg) {
 	// set up the chain with orderer
 	var chain = client.newChain(util.channel);
 	chain.addOrderer(new Orderer(ORGS.orderer));
@@ -117,7 +262,7 @@ module.exports.setupChainWithOnlyPrimaryPeer = function(client, ORGS, orgName, p
 
 
 //initial a new chain with all peers
-module.exports.setupChainWithAllPeers = function(client, ORGS, orgName) {
+function setupChainWithAllPeers(client, ORGS, orgName) {
 	var chain = client.newChain(util.channel);
 	chain.addOrderer(new Orderer(ORGS.orderer));
 
@@ -138,161 +283,3 @@ module.exports.setupChainWithAllPeers = function(client, ORGS, orgName) {
 	
 	return chain;
 }
-
-
-function connectEventHub(eventhubs, allEventhubs, peerAddr) {
-	let eh = new EventHub();
-	eh.setPeerAddr(peerAddr);
-	eh.connect();
-	eventhubs.push(eh);
-	allEventhubs.push(eh);	
-}
-
-
-function checkTheNext(peerList) {
-	// Looking for alive peer recursively and returning its url
-	
-	// Take one from the list each time, also removing it
-	var peer = popRandom(peerList);
-	
-	return isPortAlive(getIpFromEndpoint(peer), getPortFromEndpoint(peer))
-	.then((res) => {
-		logger.debug('Find alive peer and returning its url: %s', peer);
-		return peer;
-
-	}).catch((err) => {
-		logger.debug('Peer %s is not connected, try next.', peer);
-		if (0 < peerList.length) {
-			return checkTheNext(peerList);
-		} else {
-			logger.debug('Failed in getting any alive peer, returning nothing.');
-			return null;
-		}
-	});
-}
-
-
-function getIpFromEndpoint(endpoint) {
-	if (endpoint) {
-		return endpoint.split('//')[1].split(':')[0];
-	} else {
-		return null;
-	}
-}
-
-
-//Return all keys matches keyword, not strictly
-function getKeyOfJson(jsonObj, keyword) {
-	var list = [];
-	for (let key in jsonObj) {
-		if (jsonObj.hasOwnProperty(key)) {
-			if (key.indexOf(keyword) === 0) {
-				list.push(key);
-			}
-		}
-	}
-	return list;
-}
-
-
-function getOrgAll(ORGS) {
-	// There may be multiple org in ORGS
-	return getKeyOfJson(ORGS, 'org');
-}
-
-
-function getPeerAll(ORGS) {
-	var list = [];
-	var orgList = getOrgAll(ORGS);
-	for (let i in orgList) {
-		list = list.concat(getPeerByOrg(ORGS, orgList[i]));
-	}
-	return list;
-}
-
-
-function getPeerByOrg(ORGS, org) {
-	var list = [];
-	// There may be multiple peers in an org
-	var valueOfPeers = getValueOfJson(ORGS[org], 'peer');
-	for (let i in valueOfPeers) {
-		list.push(valueOfPeers[i]['requests']);
-	}
-	return list;
-}
-
-
-function getPortFromEndpoint(endpoint) {
-	if (endpoint) {
-		return endpoint.split('//')[1].split(':')[1];
-	} else {
-		return null;
-	}
-}
-
-
-function getRandom(n,m){
-	// Equal possibility of generating integer in [n,m)
-	// Notice m is open and not included in output
-	return Math.floor(Math.random()*(m-n)+n);
-}
-
-
-//Return all values matches keyword, not strictly
-function getValueOfJson(jsonObj, keyword) {
-	var list = [];
-	for (let key in jsonObj) {
-		if (jsonObj.hasOwnProperty(key)) {
-			if (key.indexOf(keyword) === 0) {
-				list.push(jsonObj[key]);
-			}
-		}
-	}
-	return list;
-}
-
-
-function isPortAlive(host, port) {
-	logger.debug('Checking connectivity of %s:%s', host, port);
-	return new Promise((resolve, reject) => {
-		net.createConnection(port, host)
-		.on("connect", function(e) {
-			resolve(); 
-		}).on("error", function(e) {
-			reject();
-		});
-	}).catch((err) => {
-		reject();
-	});	
-}
-
-
-function popRandom(list) {
-	logger.debug('Poping cell randomly from %s', list);
-	if (list) {
-		var i = getRandom(0, list.length);
-		var component = list[i];
-		list.splice(i, 1);
-		logger.debug('Successfully pop %s, now list becomes %s', component, list);
-		return component;
-	} else {
-		logger.error('Empty list, return nothing.');
-		return null;
-	}
-}
-
-/*
-function printArg(arg) {
-	logger.debug('printArg is called back');
-	logger.debug(arg);
-}
-
-function doItFirst() {
-	return new Promise((resolve, reject) => {
-		resolve();
-	}).catch((err) => {
-		logger.debug('caught an err 2');
-		reject();
-	});	
-}
-*/
