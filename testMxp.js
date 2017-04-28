ad143de0b4d1ea07028bc048a16da512487ced49
maxipeng <maxipeng@jd.com>
2017-04-28 15:46:54 +0800

    创建

---
+++ testMxp.js
@@ -0,0 +1,9 @@
+createClient = require('./app/client/create-channel.js');
+joinClient = require('./app/client/join-channel.js');
+installClient = require('./app/client/install-chaincode.js');
+invokeClient = require('./app/client/invoke-transaction.js');
+queryClient = require('./app/client/query.js');
+var mage = require('./app/manage/create-client.js');
+
+
+mage.create();
