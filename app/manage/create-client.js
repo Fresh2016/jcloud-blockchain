b35b805929faf253193676290def26459e0c6eb6
maxipeng <maxipeng@jd.com>
2017-05-02 15:21:25 +0800

    创建

--- app/manage/create-client.js
+++ app/manage/create-client.js
@@ -15,8 +15,9 @@ var logger = ClientUtils.getLogger('join-channel');
 function createChannel(channelName){
     return  queryClient.queryOrderers(channelName)
         .then((result) => {
+            console.log('createChannel: 已经被创建');
              //if(result[0]['status'] == 'UP'){
-                 return  new Promise((resolve, reject) => resolve(true));
+                 return  new Promise((resolve, reject) => resolve("已经被创建"));
              //}else{
              //    var err ={msg:"status not is up"}
              //    util.throwError(logger, err, channelName+'status not is up');
@@ -26,6 +27,7 @@ function createChannel(channelName){
              return createClient.createChannel();
         });
 }
+exports.createChannel =createChannel;
 /**
  *  join Channel
  * @returns {Promise.<T>|*|Observable}
@@ -35,7 +37,8 @@ function joinChannel(channelName){
     return   queryClient.queryPeers(channelName)
         .then((result) => {
             //if(result[0]['status'] == 'UP'){
-            return  new Promise((resolve, reject) => resolve(true));
+            console.log('createChannel: 已经被join');
+            return  new Promise((resolve, reject) => resolve("已经被join"));
             //}else{
             //    var err ={msg:"status not is up"}
             //    util.throwError(logger, err, channelName+'status not is up');
@@ -45,7 +48,7 @@ function joinChannel(channelName){
             return  joinClient.joinChannel();
         });
 }
-
+exports.joinChannel =joinChannel;
 
 /**
  * installChaincode
@@ -55,10 +58,12 @@ function installChaincode(){
     return  installClient.installChaincode()
         .catch((err) => {
             err.errName="installChaincodeError";
-            return new Promise((resolve, reject) => reject(err));
+            console.log('installChaincode: install失败 %s',JSON.stringify(err));
+            //如果失败报错，也继续执行下一个方法instantiate
+            return new Promise((resolve, reject) => resolve(err));
         });
 }
-
+exports.installChaincode =installChaincode;
 /**
  *  instantiateChaincode
  * @returns {Promise.<T>|*|Observable}
@@ -67,10 +72,11 @@ function instantiateChaincode(){
     return  invokeClient.instantiateChaincode()
         .catch((err) => {
             err.errName="installChaincodeError";
+            console.log('instantiateChaincode: instantiate失败 %s',JSON.stringify(err));
             return new Promise((resolve, reject) => reject(err));
         });
 }
-
+exports.instantiateChaincode =instantiateChaincode;
 
 
 
@@ -90,9 +96,10 @@ exports.create =function(channelName){
           console.log('installChaincode: %j\n\n\n', response);
           return  instantiateChaincode();
         }).then((response) => {
-          console.log('qinstantiateChaincode: %j\n\n\n', response);
+          console.log('instantiateChaincode: %j\n\n\n', response);
           return  new Promise((resolve, reject) => resolve("Create success"));
         }).catch((err) => {
-            return  new Promise((resolve, reject) => resolve("Create failed"));
+            console.log('Create failed %s',JSON.stringify(err));
+            return  new Promise((resolve, reject) => reject("Create failed"));
         });
 }
