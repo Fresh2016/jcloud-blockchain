7b8df1436dadd49264fa76f35ae680bf262ac7aa
maxipeng <maxipeng@jd.com>
2017-05-04 16:46:59 +0800

    参数拦截器

--- app/config/channel/channel.js
+++ app/config/channel/channel.js
@@ -5,22 +5,26 @@ var channel={
              supplychain: {
                  name:"supplychain",
                  version:"v0",
-                 path:"github.com/supplychain"
+                 path:"github.com/supplychain",
+                 peerList:["peer0","peer2"]
 
               },
              trace: {
                  name:"trace",
                  version:"v0",
-                 path:"github.com/trace"
+                 path:"github.com/trace",
+                 peerList:["peer1","peer3"]
 
              },
              sourceproduct:{
                  name:"sourceproduct",
                  version:"v0",
-                 path:"github.com/sourceproduct"
+                 path:"github.com/sourceproduct",
+                 peerList:["peer0","peer1","peer2","peer3"]
 
              }
-         }
+         },
+         txFilePath:"./app/config/mychannel.tx"
      }
 }
 
--- app/manage/param-interceptor.js
+++ app/manage/param-interceptor.js
@@ -1,11 +1,87 @@
+var ClientUtils = require('fabric-client/lib/utils.js');
+var config =require('../config/channel/channel.js');
+var logger = ClientUtils.getLogger('param-interceptor');
+var rf=require("fs");
+// Fabric client imports
+var hfc = require('fabric-client');
+hfc.addConfigFile('./app/channel/network.json');
+
 /**
- * 参数过滤器
+ * Parameter filter
  * @param req
  */
 exports.filterParams =function(req,res){
-    if(null!=req.params && null!=req.params.channelname && ""!=req.params.channelname){
-        req.query.params['channelName'] = req.params.channelname;
+    if(req.params){
+        if( null!=req.params.channelname && ""!=req.params.channelname){
+            req.query.params['channelName'] = req.params.channelname;
+
+            setNetwork(req,res);
+            setChaincodePath(req,res);
+            setTxFileData(req,res);
+        }
+    }else{
+        req.params ={};
     }
 
 }
+/**
+ * 校验是否存在channelName
+ * @param req
+ */
+function vifchannelName(req){
+  var channelName=req.query.params['channelName'];
+}
 
+/**
+ * 设置txfile的data
+ * @param req
+ */
+function setTxFileData(req,res){
+    try{
+        var  txFilePath =config[req.query.params['channelName']].txFilePath;
+        var data=rf.readFileSync("txFilePath","utf-8");
+        req.params.txFileData = data;
+    }catch(err) {
+        logger.error('setTxFileData error');
+    }
+}
+
+
+/**
+ * 设置Chaincode的Path
+ * @param req
+ */
+function setChaincodePath(req,res){
+    try{
+        var  chaincodePath =config[req.query.params['channelName']]['chanCode'][req.query.params.chaincode.name].path;
+        req.params.chaincode.path = chaincodePath;
+    }catch(err) {
+        logger.error('setChaincodePath error');
+    }
+}
+/**
+ * 设置network
+ * @param req
+ * @param res
+ */
+function setNetwork(req,res){
+    try{
+        var  peerList =config[req.query.params['channelName']]['chanCode'][req.query.params.chaincode.name].peerList;
+        req.params.network = {};
+        req.params.network.orderer = hfc.getConfigSetting('orderer');
+        for(var i =0;i<peerList.length;i++){
+           var peer =  hfc.getConfigSetting(peerList[i]);
+            if(req.params.network[peer.assign]){
+                req.params.network[peer.assign]["num"] = req.params.network[peer.assign]["num"] +1;
+                var peerNum = "peer" +req.params.network[peer.assign]["num"];
+                req.params.network[peer.assign][peerNum] = peer;
+            } else{
+                req.params.network[peer.assign] = hfc.getConfigSetting(peer.assign);
+                req.params.network[peer.assign]["peer1"] = peer;
+                req.params.network[peer.assign]["num"] =1;
+            }
+        }
+    }catch(err) {
+        logger.error('setNetwork error');
+    }
+}
