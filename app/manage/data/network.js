374466a8b2e87a6f8c20cb4301667ed690498a61
maxipeng <maxipeng@jd.com>
2017-05-08 16:08:36 +0800

    配置文件修改

--- app/manage/data/network.js
+++ app/manage/data/network.js
@@ -51,48 +51,20 @@ var data={
 }
 
 var network = {
-		"orderer": {
-			"url": "grpcs://202.77.131.6:7050",
-			"server-hostname": "orderer0",
-			"tls_cacerts": "./app/manage/data/tls/orderer/ca-cert.pem"
-		},
+		"orderer":data.orderer,
 		"org1": {
-			"name": "peerOrg1",
-			"mspid": "Org1MSP",
-			"ca": "https://202.77.131.6:7054",
-			"peer1": {
-				"requests": "grpcs://202.77.131.6:7051",
-				"events": "grpcs://202.77.131.6:7053",
-				"server-hostname": "peer0",
-				"tls_cacerts": "./app/manage/data/tls/peers/peer0/ca-cert.pem",
-				"isAnchor": "true"
-			},
-			"peer2": {
-				"requests": "grpcs://202.77.131.6:7056",
-				"events": "grpcs://202.77.131.6:7058",
-				"server-hostname": "peer1",
-				"tls_cacerts": "./app/manage/data/tls/peers/peer1/ca-cert.pem",
-				"isAnchor": "true"
-			}
+			"name": data.org1.name,
+			"mspid":data.org1.mspid,
+			"ca": data.org1.ca,
+			"peer1": data.peer0,
+			"peer2": data.peer1
 		},
 		"org2": {
-			"name": "peerOrg2",
-			"mspid": "Org2MSP",
-			"ca": "https://202.77.131.6:8054",
-			"peer1": {
-				"requests": "grpcs://202.77.131.6:8051",
-				"events": "grpcs://202.77.131.6:8053",
-				"server-hostname": "peer2",
-				"tls_cacerts": "./app/manage/data/tls/peers/peer2/ca-cert.pem",
-				"isAnchor": "true"
-			},
-			"peer2": {
-				"requests": "grpcs://202.77.131.6:8056",
-				"events": "grpcs://202.77.131.6:8058",
-				"server-hostname": "peer3",
-				"tls_cacerts": "./app/manage/data/tls/peers/peer3/ca-cert.pem",
-				"isAnchor": "true"
-			}
+			"name": data.org2.name,
+			"mspid":  data.org2.mspid,
+			"ca":  data.org2.ca,
+			"peer1": data.peer2,
+			"peer2": data.peer3
 		}
 }
 
